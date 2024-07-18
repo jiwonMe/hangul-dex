a544069eb4bcedeb2d575c2fe7809a1d374c2f28
jiwonMe <park@jiwon.me>
2024-07-18 22:41:16 +0900

    feat: 상용한글모드

--- src/app/_components/RandomCharacter.tsx
+++ src/app/_components/RandomCharacter.tsx
@@ -3,8 +3,12 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { 상용한글string } from './상용한글string';
 
-const getRandomCharacter = () => {
+const getRandomCharacter = (only상용한글?: boolean) => {
+  if (only상용한글) {
+    return 상용한글string.charAt(Math.floor(Math.random() * 상용한글string.length));
+  }
   // Generate a random Korean character between '가' and '힣'
   const start = '가'.charCodeAt(0);
   const end = '힣'.charCodeAt(0);
@@ -16,16 +20,20 @@ const getRandomCharacter = () => {
  * 랜덤 한글 문자를 반환하는 컴포넌트
  * @returns 
  */
-const RandomCharacter = () => {
+const RandomCharacter = ({
+  only상용한글 = false
+}: {
+  only상용한글: boolean
+}) => {
   const [character, setCharacter] = useState<string>('');
 
   useEffect(() => {
-    const randomCharacter = getRandomCharacter();
+    const randomCharacter = getRandomCharacter(only상용한글);
     setCharacter(randomCharacter);
   }, []);
 
   const handleClick = () => {
-    setCharacter(getRandomCharacter());
+    setCharacter(getRandomCharacter(only상용한글));
   }
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-zinc-100">
       <h1 className="text-md text-semibold text-zinc-500">당신의 한글은?</h1>
-      <RandomCharacter />
+      <RandomCharacter only상용한글={true} />
     </main>
   );
 }
