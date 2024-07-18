aa744029913b9ceb1df009b08da636fee65e0b21
jiwonMe <park@jiwon.me>
2024-07-18 22:54:08 +0900

    feat: 로고 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import RandomCharacter from "./_components/RandomCharacter";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-zinc-100">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-100">
+      <div className="flex flex-col items-center">
       <h1 className="text-md text-semibold text-zinc-500">당신의 한글은?</h1>
       <RandomCharacter only상용한글={true} />
+      </div>
+      <Image src="/한글도감_로고.svg" alt="한글도감 로고" width={80} height={35} />
     </main>
   );
 }
