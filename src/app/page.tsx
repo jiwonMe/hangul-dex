9fc316af211f445bbe932d49f975f2fe188529f2
jiwonMe <park@jiwon.me>
2024-07-18 23:04:43 +0900

    feat: add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-100">
       <div className="flex flex-col items-center">
-      <h1 className="text-md text-semibold text-zinc-500">당신의 한글은?</h1>
+      <h1 className="text-md text-semibold text-zinc-500 maruburi">지금의 글자</h1>
       <RandomCharacter only상용한글={true} />
       </div>
       <Image src="/한글도감_로고.svg" alt="한글도감 로고" width={80} height={35} />
